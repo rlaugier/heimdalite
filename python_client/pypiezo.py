4460bc9d5d17b6527773ca5bb141771d62ce2558
KwintenMissiaen <kwinten.missiaen@kuleuven.be>
2025-06-10 11:20:46 +0200

    A few small fixes to communication; added command for verbose mode; show output from the arduino

--- python_client/pypiezo.py
+++ python_client/pypiezo.py
@@ -9,6 +9,8 @@ Base example:
 
 import numpy as np
 import serial
+import threading
+import time
 gain = 4000.0/9.5e-6
 gains = gain*np.ones(4)
 port_params = {"port":"/dev/ttyACM0", "baudrate":9600,
@@ -30,11 +32,24 @@ class piezointerface(object):
         self.gains = gains
         self.raw_values = self.values2raw(self.values)
 
+        self.listening = True
+        listen_thread = threading.Thread(target=self.listen)
+        listen_thread.start()
+    
+    def listen(self):
+        self.ser.timeout = 0.1
+        while(self.listening):
+            answer = self.ser.read_until()
+            if(len(answer) > 0):
+                print(answer.decode("utf-8"))
+
     def __del__(self):
         print("Reseting server")
         self.reset_server()
         print("Closing the interface")
-        self.res.close()
+        self.listening = False
+        time.sleep(.01)
+        self.ser.close()
         print("done")
         
     def get_raw_values(self):
@@ -56,6 +71,12 @@ class piezointerface(object):
     def reset_server(self,):
         self.ser.write(b"[z,0]")
     
+    def set_verbose_mode(self, val: bool):
+        if val:
+            self.ser.write(b"[v,1]")
+        else:
+            self.ser.write(b"[v,0]")
+
     def _send(self):
         myrawvalues = self.raw_values
         self.ser.write(self.vals2bytes("s",myrawvalues))
@@ -65,7 +86,7 @@ class piezointerface(object):
             if any_values.dtype == int:
                 self.raw_values = any_values
                 self.values = self.raw2values(self.raw_values)
-            elif any_values.dtype == float:
+            elif any_values.dtype == float or any_values.dtype == np.float64:
                 thevalues = any_values
                 self.values = thevalues
                 self.raw_values = self.raw2values(self.values)
@@ -79,10 +100,10 @@ class piezointerface(object):
         return newraws
         
     def vals2bytes(self, mystring, myarray):
-        newstring = mystring
+        newstring = "[" + mystring
         for i in np.arange(self.n):
             newstring = newstring + ","
             newstring = newstring + f"{myarray[i]}"
-        newstring = newstring + "\n"
+        newstring = newstring + "]" + "\n"
         bytearray = newstring.encode("utf-8")
         return bytearray
